ae6619954568952901037b959c4d5d3d89819c73
DerrickJames <dmushangi@gmail.com>
2015-11-27 10:18:17 +0300

    Synching repos on the client-side

--- resources/assets/js/controllers/reposController.js
+++ resources/assets/js/controllers/reposController.js
@@ -4,7 +4,23 @@
 
 	angular
 		.module('projectManager')
-		.controller('ReposController', function() {
-			// TODO: get repos
+		.controller('ReposController', function($scope, $http) {
+			$scope.repos;
+			$scope.error;
+			$scope.loading = true;
+
+			$http
+				.get('/api/sync/github')
+				.then(function(repos) {
+                    console.log('Repos: ', repos);
+
+					$scope.loading = false;
+					$scope.repos   = repos.data.repos;
+				})
+
+				.catch(function(error) {
+					$scope.loading = false;
+					$scope.error   = error;
+				});
 		});
 })();
