184fca63a8b78fdde8d3b4ed5729a1be99d91801
DerrickJames <dmushangi@gmail.com>
2015-11-27 09:55:29 +0300

    Adding dingo/api for routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,25 @@
 |
 */
 
+/*
+|-------------------------------------------
+| API's Endpoints
+|-------------------------------------------
+*/
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function($api) {
+	$api->get('sync/github', 'App\Http\Controllers\Api\V1\SyncController@getAllRepos');
+    $api->post('auth/github', 'App\Http\Controllers\Api\V1\AuthenticateController@authenticate');
+    $api->get('auth/user', 'App\Http\Controllers\Api\V1\AuthenticateController@getAuthenticatedUser');
+});
+
+/*
+|------------------------------------------
+| Main Routes
+|------------------------------------------
+*/
+
 Route::get('/', function () {
     return view('index');
 });
-
-Route::post('auth/github', 'Api\V1\AuthenticateController@authenticate');
