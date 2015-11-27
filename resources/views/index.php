f68259f7f777e1961bb6d8a9e09881162969e09c
DerrickJames <dmushangi@gmail.com>
2015-11-27 10:19:18 +0300

    Including the navBar

--- resources/views/index.php
+++ resources/views/index.php
@@ -2,25 +2,13 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <base href="/">
         <title>Project Manager</title>
         <link rel="stylesheet" href="/assets/css/app.css">
     </head>
     <body ng-app="projectManager">
-
-        <nav class="navbar navbar-default">
-          <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">Project Manager</a>
-            </div>
-          </div><!-- /.container-fluid -->
-        </nav>
+        
+        <div data-ng-include="'views/navBar.html'"></div>
 
         <div class="container">
             <div ui-view></div>
