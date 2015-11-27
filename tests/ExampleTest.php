2d25d0b59dda537e847282d8573f8cb028ded1e1
DerrickJames <dmushangi@gmail.com>
2015-11-27 10:19:48 +0300

    Tinkering with initial tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,7 +13,23 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+  //      $this->visit('/')
+        //           ->see('Laravel 5');
+        
+        $response = $this->call('GET', '/');
+
+        $this->assertEquals(200, $response->status());
+    }
+
+    /**
+     * Another basic functional text example.
+     *
+     * @return void
+     */
+    public function testAnotherBasicExample()
+    {
+        $this->visit('/')->see('Project Manager');
+
+ //       $this->assertResponseOk();
     }
 }
