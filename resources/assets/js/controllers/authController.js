0d09f860a9d20d00579e740547e1b01e712567a4
DerrickJames <dmushangi@gmail.com>
2015-11-27 10:17:35 +0300

    Adding logout logic

--- resources/assets/js/controllers/authController.js
+++ resources/assets/js/controllers/authController.js
@@ -4,16 +4,44 @@
 
 	angular
 		.module('projectManager')
-		.controller('AuthController', function($scope, $auth, $state) {
-			$scope.authenticate = function(provider) {
-				$auth.authenticate(provider)
-					.then(function(response) {
-						$state.go('repos', {});
-					})
+		.controller('AuthController', function($scope, $auth, $state, $http, $rootScope) {
+            $scope.error;
+            $scope.loading = true;
 
-					.catch(function(error) {
-						console.log('Errors: ', error);
+            $scope.authenticate = function(provider) {
+				$auth
+					.authenticate(provider)
+					.then(function(response) {
+                        console.log('Response: ', response);
+                        return $http.get('/api/auth/user');
+
+					}, function(error) {
+                        $scope.loading = false;
+                        $scope.error   = error;
+                    })
+
+					.then(function(response) {
+						console.log('GetResponse: ', response);
+                        
+                        var user = JSON.stringify(response.data.user);
+
+                        localStorage.setItem('ProjectManager.currentUser', user);
+
+                        $rootScope.authenticated = true;
+                        $rootScope.currentUser   = response.data.user;
+                        
+                        $state.go('repos');
 					});
 			}
+
+            $scope.logout = function() {
+                console.log('Clicked');
+                $auth.logout().then(function() {
+                    localStorage.removeItem('ProjectManager.currentUser');
+
+                    $rootScope.authenticated = false;
+                    $rootScope.currentUser   = null;
+                });
+            }
 		});
 })();
