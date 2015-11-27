527b9f525a1e5f8425e30a0c1993ed8770a54ad7
DerrickJames <dmushangi@gmail.com>
2015-11-27 10:14:53 +0300

    Updating client-side build

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -4,9 +4,37 @@
 
 	angular
 		.module('projectManager', ['ui.router', 'satellizer'])
-		.config(function($stateProvider, $urlRouterProvider, $authProvider) {
+		.config(function($stateProvider, $urlRouterProvider, $authProvider, $httpProvider, $provide, $locationProvider) {
+
+            function requireLoginHook($q, $injector) {
+                return {
+                    responseError: function(rejection) {
+                        var $state        = $injector.get('$state'),
+
+                            errorStatuses = [
+                                'token_absent', 'token_invalid',
+                                'token_not_provided', 'token_expired'
+                            ];
+                        
+                        angular.forEach(errorStatuses, function(value, key) {
+                            if(rejection.data.error === value) {
+                                localStorage.removeItem('ProjectManager.currentUser');
+
+                                $state.go('auth');
+                            }
+                        });
+
+                        return $q.reject(rejection);
+                    }
+                };
+            }
+
+            $provide.factory('requireLoginHook', requireLoginHook);
+
+            $httpProvider.interceptors.push('requireLoginHook');
 
 			$authProvider.github({
+				url: '/api/auth/github',
 		      	clientId: '14ceda48bc30a9f5acf5'
 		    });
 
@@ -22,36 +50,103 @@
 					templateUrl: '../views/reposView.html',
 					controller: 'ReposController'
 				});
+				
 			$urlRouterProvider.otherwise('/');
+            $locationProvider.html5Mode(true);
 		})
+
+        .run(function($rootScope, $state) {
+            $rootScope.$on('$stateChangeStart', function(event, toState) {
+                var userString  = localStorage.getItem('ProjectManager.currentUser'),
+                    currentUser = JSON.parse(userString);
+
+                if(currentUser) {
+                    $rootScope.authenticated = true;
+                    $rootScope.currentUser   = currentUser;
+
+                    if(toState.name === 'auth') {
+                        event.preventDefault();
+
+                        $state.go('repos');
+                    }
+                }
+            });
+        });
 })();
+
 (function() {
 
 	'use strict';
 
 	angular
 		.module('projectManager')
-		.controller('AuthController', function($scope, $auth, $state) {
-			$scope.authenticate = function(provider) {
-				$auth.authenticate(provider)
-					.then(function(response) {
-						$state.go('repos', {});
-					})
+		.controller('AuthController', function($scope, $auth, $state, $http, $rootScope) {
+            $scope.error;
+            $scope.loading = true;
 
-					.catch(function(error) {
-						console.log('Errors: ', error);
+            $scope.authenticate = function(provider) {
+				$auth
+					.authenticate(provider)
+					.then(function(response) {
+                        console.log('Response: ', response);
+                        return $http.get('/api/auth/user');
+
+					}, function(error) {
+                        $scope.loading = false;
+                        $scope.error   = error;
+                    })
+
+					.then(function(response) {
+						console.log('GetResponse: ', response);
+                        
+                        var user = JSON.stringify(response.data.user);
+
+                        localStorage.setItem('ProjectManager.currentUser', user);
+
+                        $rootScope.authenticated = true;
+                        $rootScope.currentUser   = response.data.user;
+                        
+                        $state.go('repos');
 					});
 			}
+
+            $scope.logout = function() {
+                console.log('Clicked');
+                $auth.logout().then(function() {
+                    localStorage.removeItem('ProjectManager.currentUser');
+
+                    $rootScope.authenticated = false;
+                    $rootScope.currentUser   = null;
+                });
+            }
 		});
 })();
+
 (function() {
 
 	'use strict';
 
 	angular
 		.module('projectManager')
-		.controller('ReposController', function() {
-			// TODO: get repos
+		.controller('ReposController', function($scope, $http) {
+			$scope.repos;
+			$scope.error;
+			$scope.loading = true;
+
+			$http
+				.get('/api/sync/github')
+				.then(function(repos) {
+                    console.log('Repos: ', repos);
+
+					$scope.loading = false;
+					$scope.repos   = repos.data.repos;
+				})
+
+				.catch(function(error) {
+					$scope.loading = false;
+					$scope.error   = error;
+				});
 		});
 })();
+
 //# sourceMappingURL=application.js.map
